ab34e6e9d2e43bde9240ac1d9d6f7b8716051495
N30n <105752851+Kawsar-987@users.noreply.github.com>
2023-06-09 01:22:45 +0600

    Update LCS.c

--- LCS.c
+++ LCS.c
@@ -21,11 +21,14 @@ int Lcs_length(char* str1, char* str2, int m, int n){
     return Lcs[m][n];
 }
 int main(){
-    char str1[100],str2[100];
-    printf("Enter your 1st sequence :\n");
-    scanf("%s",&str1);
-    printf("Enter your 2nd sequence :\n");
-    scanf("%s",&str2);
+     printf("Enter the first string: ");
+    fgets(str1, sizeof(str1), stdin);
+    str1[strcspn(str1, "\n")] = '\0';
+
+    printf("Enter the second string: ");
+    fgets(str2, sizeof(str2), stdin);
+    str2[strcspn(str2, "\n")] = '\0';
+
     int m= strlen(str1);
     int n= strlen(str2);
     int Lcs_leng= Lcs_length(str1,str2,m,n);
